b132deeb38e93ce822363e9f7a665744bebd7203
Maulana-king <agusrohim56@gmail.com>
2024-10-16 10:38:39 +0700

    add function to check what kind of operator user want, and use list of function as callback

--- calculator.js
+++ calculator.js
@@ -12,3 +12,21 @@ function divide(a,b) {
     return a / b;
 }
 
+let numFirst;
+let operator;
+let numSecond;
+
+function operate(numOne, operator, numTwo) {
+    switch(operator) {
+        case '+':
+            return add(numOne, numTwo);
+        case '-':
+            return substract(numOne, numTwo);
+        case '*':
+            return multiply(numOne, numTwo);
+        case '/':
+            return divide(numOne, numTwo);
+        default:
+            return 'Please enter the operator Math (+,-,*,/)';
+    }
+}
