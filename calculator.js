b408895b5f9a944806dafcc8ce7ddd00809c9f10
Maulana-king <agusrohim56@gmail.com>
2024-10-16 12:43:59 +0700

    add function operator and clear

--- calculator.js
+++ calculator.js
@@ -31,10 +31,13 @@ function operate(numOne, operator, numTwo) {
     }
 }
 
-const inputNumber = document.querySelector('.input-number span');
+// get the input element in html
+const displayNumber = document.querySelector('.input-number span');
 let collectNumber = '';
 
+// get the button number in html
 const buttonDigit = document.querySelectorAll('.digit button');
+
 // When user click the button number
 buttonDigit.forEach((digit) => {
     digit.addEventListener('click', (number) => {
@@ -42,14 +45,76 @@ buttonDigit.forEach((digit) => {
         let getNumber = number.target.innerText;
         collectNumber += getNumber;
 
-        inputNumber.textContent = collectNumber;
+        displayNumber.textContent = collectNumber;
     });
 });
 
 
+// get display h2 operator in html
+let displayOperator = document.querySelector('.input-operator span');
+// get the operator button in html
+const buttonOperators = document.querySelectorAll('.operators button');
+let inputOperator = '';
+//assign first number when user click operate button
+let firstNumber = '';
+// when user click one of the operator button
+buttonOperators.forEach((operator) => {
+    operator.addEventListener('click', (oper) => {
+        inputOperator = oper.target.innerText;
+        displayOperator.textContent = inputOperator;
+        //assign value number first 
+        firstNumber = collectNumber;
+        //reset to the zero or empty
+        collectNumber ='';
+        //remove the inner text for remove number in display
+        displayNumber.textContent = '';
+    });
+});
+
+
+//get equal or solution button element in HTML
+const buttonEqual = document.querySelector('.equals');
+//assing the number when user click the equal button
+let secondNumber = '';
+//get the result element in HTML
+const resultDisplay = document.querySelector('.result-calculator span');
+//assign the result from function
+let resultTotal = '';
+buttonEqual.addEventListener('click', () => {
+    secondNumber = collectNumber;
+
+    //remove all display input and operator
+    displayNumber.textContent = '';
+    displayOperator.textContent = '';
+
+    //console.log(typeof firstNumber)
+    // console.log(inputOperator)
+    // console.log(secondNumber)
+    
+    //make the value of each number to type number before throw as arguments
+    let firstN = Number(firstNumber);
+    let secondN = Number(secondNumber);
+
+    //invoke the calculator function
+    resultTotal = operate(firstN, inputOperator, secondN);
+    //show the result to the html
+    resultDisplay.textContent = resultTotal;
+});
+
+
+
+
+
+
+//When user clik clear button
 const buttonClear = document.querySelector('.clear');
 buttonClear.addEventListener('click', () => {
     collectNumber = '';
-
-    inputNumber.textContent = collectNumber;
-})
+    resultTotal = '';
+    inputOperator = '';
+    firstNumber = '';
+    secondNumber = '';
+    displayNumber.textContent = '';
+    resultDisplay.textContent = '';
+    displayOperator.textContent = '';
+});
